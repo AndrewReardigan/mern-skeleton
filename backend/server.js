21af060b1d4c4523504ebc4b50b9a466d1b5e6d5
andrewreardigan <andrewreardigan@icloud.com>
2020-09-13 19:17:34 -0400

    _

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,30 @@
+const express = require('express')
+const app = express()
 
+const port = process.env.PORT || 3000 
+
+app.use(express.json())
+
+
+
+// server.js
+
+/**
+ * Required External Modules
+ */
+
+/**
+ * App Variables
+ */
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+
+/**
+ * Server Activation
+ */
